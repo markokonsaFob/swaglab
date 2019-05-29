53c118d2230b078b21fd76dafee2f3617291e06b
Marko Konsa <ext-marko.konsa@here.com>
2019-05-29 09:17:44 +0300

    Add ActionWrapper methods that will be needed

--- src/test/groovy/impl/helpers/ActionsWrapper.groovy
+++ src/test/groovy/impl/helpers/ActionsWrapper.groovy
@@ -1,9 +1,16 @@
 package impl.helpers
 
+import io.appium.java_client.AppiumDriver
+import io.appium.java_client.TouchAction
+import io.appium.java_client.touch.WaitOptions
+import io.appium.java_client.touch.offset.PointOption
 import io.cify.framework.core.Device
+import org.openqa.selenium.Dimension
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.support.ui.ExpectedCondition
 import org.openqa.selenium.support.ui.WebDriverWait
+
+import java.time.Duration
 
 /**
  * Created by Marko Konsa for Nordic Testing Day workshop
@@ -34,6 +41,42 @@ class ActionsWrapper {
             })
         } catch (all) {
             throw new CifyTestException("Waiting for condition failed! " + "CONDITION: " + condition + " cause " + all.message)
+        }
+    }
+
+    /**
+     * Slowly scroll down on screen
+     * @param device
+     */
+    static void slowScrollDown(Device device) {
+        TouchAction action = new TouchAction(device.getDriver() as AppiumDriver)
+        Dimension dimension = device.getDriver().manage().window().getSize()
+        action.press(
+                PointOption.point(
+                        (dimension.getWidth() / 4).toInteger(),
+                        (dimension.getHeight() * 0.75).toInteger()
+                )
+        ).waitAction(
+                WaitOptions.waitOptions(
+                        Duration.ofMillis(2000)
+                )
+        ).moveTo(
+                PointOption.point(
+                        (dimension.getWidth() / 4).toInteger(),
+                        (dimension.getHeight() * 0.25).toInteger()
+                )
+        ).release().perform()
+    }
+
+    /**
+     * Hides mobile keyboard
+     * @param device
+     */
+    static void hideKeyboard(Device device) {
+        try {
+            (device.getDriver() as AppiumDriver).hideKeyboard()
+        } catch (ignored) {
+
         }
     }
 }
